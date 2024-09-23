da8fe7c37591edd8c869fdebebcd29eb05eeefc5
jayen123 <jayenhasan2003@gmail.com>
2024-09-24 03:28:03 +0600

    added cash out functionality for cash out

--- js/cashOut.js
+++ js/cashOut.js
@@ -1,4 +1,17 @@
 document.getElementById("cashOut").addEventListener("click", function (event) {
   event.preventDefault();
-  
+
+  const cashOutNumber = getInputValueByID("input-cash-Out");
+  const pinNumber = getInputValueByID("input-pin-cashOut");
+
+  const currentBalance = document.getElementById("current-balance").innerText;
+  const currentBalanceNumber = parseFloat(currentBalance);
+
+  if (pinNumber !== 1234) {
+    alert("Failed to Cash out! please try again later");
+  }
+
+  const newBalance = currentBalanceNumber - cashOutNumber;
+
+  document.getElementById("current-balance").innerText = newBalance;
 });
